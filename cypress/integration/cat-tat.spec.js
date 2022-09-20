71cbfc76ee4b3a55b3a884076d9190786af78a33
Igor Trentini <igortrentini.2004@gmail.com>
2022-09-20 13:01:35 -0300

    feat: Teste preencher dados obrigatorios formulario

--- cypress/integration/cat-tat.spec.js
+++ cypress/integration/cat-tat.spec.js
@@ -1,9 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-    it('Verifica o título da aplicação', () => {
-        cy.visit('src/index.html');
+    beforeEach(() => cy.visit('src/index.html'));
 
+    it('Verifica o título da aplicação', () => {
         cy.title().should('equal', 'Central de Atendimento ao Cliente TAT');
+    });
+
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('Igor');
+        cy.get('#lastName').type('Trentini');
+        cy.get('#email').type('igortrentini@email.com');
+        cy.get('#open-text-area').type('Obrigado pelo bom trabalho!');
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('.success').should('be.visible');
     });
 });
